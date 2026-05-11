df2995a57bcf5a74088c30ba0c05d6de11c9d348
hansikahj9-pixel <hansikahj9@gmail.com>
2026-05-11 23:21:43 +0530

    fix: enable navigation for Shakespearean Love project in ProjectList

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -76,6 +76,8 @@ export default function ProjectList({ onHover }: ProjectListProps) {
             onClick={() => {
               if (project.name === 'AXIOME') {
                 navigate('/axiome');
+              } else if (project.name === 'Shakespearean Love') {
+                navigate('/shakespeare');
               }
             }}
             onMouseEnter={(e) =>
